0538f951218c877fddeba8fd766440893995e612
viperluan <viperluan@gmail.com>
2021-06-06 11:23:20 -0300

    The axios library was added, the server script was fixed and the first test using axios was done.

--- games-critical-score/src/App.js
+++ games-critical-score/src/App.js
@@ -1,7 +1,22 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 import Card from './components/Card';
 import Header from './components/Header';
 
 function App() {
+  const [gameList, setGameList] = useState([]);
+
+  useEffect(() => {
+    const getGameList = async () => {
+      const response = await axios.get('http://localhost:8000/games');
+
+      console.log(response.data);
+    };
+
+    getGameList();
+  }, []);
+
   return (
     <>
       <Header />
